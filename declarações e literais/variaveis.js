bc23cf818989bfdf60bbbc36986bf4450835c4e3
cesinha1 <cesardepaula80@gmail.com>
2023-04-27 14:48:20 -0300

    declarações e literais

---
+++ declarações e literais/variaveis.js
@@ -0,0 +1,18 @@
+var nomePessoa = 'cesar';
+console.log(nome);
+
+let nome = 'eike';
+console.log(nomeCachorro);
+
+const nomeAviao = 'mirrage';
+console.log(nomeAviao);
+
+function myName(){
+    var name = 'cesar';
+}
+myName();
+
+
+// var - Global
+// let - Escopo
+// const - Constante
